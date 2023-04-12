c263f9d2f5cb0bff76e85eaa5cc934c9e2cd36c0
Flavio <suporte.flavio.souza@hotmail.com>
2023-04-12 15:19:40 -0300

    Project create

---
+++ src/test/java/com/teste/manager/systems/TesteProjetoManagerSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.manager.systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteProjetoManagerSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
